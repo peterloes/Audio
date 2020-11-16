d2ed2048382871a9cdd0c0f6536b194507151160
Peter Loes <loes@orn.mpg.de>
2020-11-16 14:19:22 +0100

    Add files via upload

--- Software/drivers/CfgData.h
+++ Software/drivers/CfgData.h
@@ -2,10 +2,12 @@
  * @file
  * @brief	Header file of module CfgData.c
  * @author	Ralf Gerhauser
- * @version	2018-11-13
+ * @version	2018-03-25
  ****************************************************************************//*
 Revision History:
-2018-11-13,rage	- Added new data type CFG_VAR_TYPE_INT_GE_1.
+2018-03-25,rage	- Added ENUM_DEF to be able to use ENUM definitions.
+		- Defined CFG_VAR_TYPE_ENUM_1 to 5.
+		- Changed prototype for CfgDataInit().
 2017-05-02,rage	- Renamed Cam1Duration to CamDuration.
 		- Added data types CFG_VAR_TYPE_INTEGER and CFG_VAR_TYPE_CONFIG.
 2017-01-25,rage	Initial version.
@@ -23,11 +25,11 @@ Revision History:
     /*!@brief Configuration variable data types. */
 typedef enum
 {
-    CFG_VAR_TYPE_TIME,		// 00:00 to 23:59
-    CFG_VAR_TYPE_DURATION,	// 0 to n seconds, or A for always
-    CFG_VAR_TYPE_ID,		// transponder ID with optional parameters
-    CFG_VAR_TYPE_INTEGER,	// positive integer variable 0..n
-    CFG_VAR_TYPE_CONFIG,	// configuration data
+    CFG_VAR_TYPE_TIME,		//!< 00:00 to 23:59
+    CFG_VAR_TYPE_DURATION,	//!< 0 to n seconds
+    CFG_VAR_TYPE_ID,		//!< transponder ID with optional parameters
+    CFG_VAR_TYPE_INTEGER,	//!< positive integer variable (0 to n)
+    CFG_VAR_TYPE_CONFIG,	//!< configuration data
     CFG_VAR_TYPE_ENUM_1,	//!< enumeration 1
     CFG_VAR_TYPE_ENUM_2,	//!< enumeration 2
     CFG_VAR_TYPE_ENUM_3,	//!< enumeration 3
@@ -37,14 +39,14 @@ typedef enum
 } CFG_VAR_TYPE;
 
     /*!@brief Special states for @ref CFG_VAR_TYPE_DURATION. */
-#define DUR_INVALID (-1)	// entry is invalid
+#define DUR_INVALID (-1)	//!< entry is invalid
 
     /*!@brief Structure to define configuration variables. */
 typedef struct
 {
-    const char	      *name;	// variable name
-    const CFG_VAR_TYPE type;	// variable type
-    const void	      *pData;	// address of data variable
+    const char	      *name;	//!< variable name
+    const CFG_VAR_TYPE type;	//!< variable type
+    const void	      *pData;	//!< address of data variable
 } CFG_VAR_DEF;
 
     /*!@brief Type for a list of enum definitions. */
@@ -57,7 +59,7 @@ typedef struct _ID_PARM
     int32_t  KeepPlayback;	// individual KEEP_PLAYBACK duration
     int32_t  KeepRecord;	// individual KEEP_RECORD duration
     int32_t  PlayType;	        // individual PLAYBACK_TYPE
-    char     ID[];		// ID string follows
+    char     ID[];		//!< ID string follows
 } ID_PARM;
 
 
